c8464f85420ea72f44a3db0d2748be8c95e22d70
Leo W <leo.westebbe@gmail.com>
2022-10-19 17:33:06 -0400

    App displays local weather conditions based on zip

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,19 +1,48 @@
-// FOR TESTING
-import testJson from "./openWeatherTest.json" assert { type: "json" };
-const current = testJson["current"];
-const date = new Date(current["dt"] * 1000);
-
 // creates a new div in the body
-const appendDivToBody = (id, innerHTML) => {
+export const appendDivToBody = (id, innerHTML) => {
   const newElement = document.body.appendChild(document.createElement("div"));
   newElement.id = id;
   newElement.innerHTML = innerHTML;
   return newElement;
 };
 
-const createTimeDiv = appendDivToBody("time", date);
-const jsonFormattingTestDiv = appendDivToBody(
-  "jsonTestDiv",
-  jsonStringConstructor(testJson, "current", "timezone")
-);
+// builds a list from a given object (param 1) within a given ul element (param 2)
+export const listItemsFromObject = (object, listElement) => {
+  for (const property in object) {
+    const newLiItem = listElement.appendChild(document.createElement("li"));
+    newLiItem.innerHTML = `${property}: ${object[property]}`;
+  }
+};
 
+export const formatCurrentData = (object) => {
+  // Items for use within new object
+  const date = new Date(object["dt"] * 1000);
+  const sunriseDate = new Date(object["sunrise"] * 1000);
+  const sunsetDate = new Date(object["sunset"] * 1000);
+  const weather = object["weather"][0];
+
+  // to deal with minute values under 10
+  const fixMinutes = (getMinutesVal) => {
+    if (getMinutesVal < 10) {
+      return `0${getMinutesVal}`;
+    } else {
+      return getMinutesVal;
+    }
+  };
+
+  // build object to display in list
+  const newObject = {
+    Date: date,
+    Sunrise: `${sunriseDate.getHours()}:${fixMinutes(
+      sunriseDate.getMinutes()
+    )}`,
+    Sunset: `${sunsetDate.getHours()}:${fixMinutes(sunsetDate.getMinutes())}`,
+    Temperature: object["temp"] + " F",
+    "Feels like": object["feels_like"] + " F",
+    Humidity: object["humidity"] + "%",
+    "UV index": object["uvi"],
+    "Wind speed": object["wind_speed"] + "mph",
+    Weather: `${weather["main"]} (${weather["description"]})`,
+  };
+  return newObject;
+};
--- main.js
+++ main.js
@@ -1,27 +1,69 @@
+import { listItemsFromObject, formatCurrentData } from "./helperFunctions.js";
+
+// API CALL COMPONENTS
+// Global:
 const apiKey = "1590f969c740e8816dfe62664548bb03";
-const zipInput = document.querySelector("#zipInput");
-const zipcode = zipInput.value;
 
-// Geolocation API call components
-const geolocationUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${countrycode}&appid=${apiKey}`;
+// Geolocation API:
+const geolocationBaseUrl = "https://api.openweathermap.org/geo/1.0";
 
-// Weather API call components
-const lat = "71.06";
-const lon = "42.36";
+// Weather API:
+const localDataBaseUrl = "https://api.openweathermap.org/data/3.0";
 const units = "imperial";
 const part = "minutely,hourly,daily,alerts";
-const openWeatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=${part}&appid=${apiKey}`;
 
-// get main body elements
+// store html elements
 const weatherSection = document.getElementById("weatherInfo");
+const clothingOptions = document.getElementById("clothingOptions");
+const localDataList = document.getElementById("localDataList");
+const testList = document.getElementById("testList");
+const zipInput = document.querySelector("#zipInput");
+const countryCodeInput = document.querySelector("#countryCodeInput");
+const submit = document.querySelector("#submit");
 
-/* This function costs money. uncomment as needed.
-const testCall = async () => {
-  const response = await fetch(openWeatherUrl);
-  if (response.ok) {
-    const jsonResponse = await response.json();
-    console.log(jsonResponse["timezone"])
-    return jsonResponse;
+// Find coordinates based on zipcode.
+const getCoordinates = async (event) => {
+  event.preventDefault(); //do not clear page after submit
+  // build fetch url
+  const zipCode = zipInput.value;
+  const countryCode = countryCodeInput.value;
+  const zipQuery = `/zip?zip=${zipCode},${countryCode}`;
+  const authQuery = `&appid=${apiKey}`;
+  const fetchUrl = `${geolocationBaseUrl}${zipQuery}${authQuery}`;
+  // process fetch data
+  try {
+    const response = await fetch(fetchUrl);
+    if (response.ok) {
+      const responseObject = await response.json();
+      const lat = responseObject["lat"];
+      const lon = responseObject["lon"];
+      getLocalData(lat, lon);
+    } else {
+      alert("Please enter valid zipcode and country code.");
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
-*/
+
+/* This function costs money. uncomment as needed.*/
+const getLocalData = async (lat, lon) => {
+  // build fetch url
+  const coordinateQuery = `/onecall?lat=${lat}&lon=${lon}`;
+  const paramsQuery = `&units=${units}&exclude=${part}`;
+  const authQuery = `&appid=${apiKey}`;
+  const fetchUrl = `${localDataBaseUrl}${coordinateQuery}${paramsQuery}${authQuery}`;
+  // process fetch data
+  try {
+    const response = await fetch(fetchUrl);
+    if (response.ok) {
+      const responseObject = await response.json();
+      const formattedObject = formatCurrentData(responseObject["current"]);
+      listItemsFromObject(formattedObject, localDataList);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+submit.addEventListener("click", getCoordinates);
